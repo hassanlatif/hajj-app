2e488bff7b87cdd36259b6395c123e2d4711a3cf
Hassan Mughal <hassan.mughal@gmail.com>
2017-07-30 20:02:22 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,14 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { Geolocation } from '@ionic-native/geolocation';
+
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+// import { GoogleMapsProvider } from '../providers/google-maps/google-maps';
+// import { ConnectivityProvider } from '../providers/connectivity/connectivity';
+// import { GoogleMapsClusterProvider } from '../providers/google-maps-cluster/google-maps-cluster';
 
 @NgModule({
   declarations: [
@@ -24,7 +29,11 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    Geolocation,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    // GoogleMapsProvider,
+    // ConnectivityProvider,
+    // GoogleMapsClusterProvider
   ]
 })
 export class AppModule {}
